d23e58dc1b72797232f2bffa602dd0e174f6086d
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-07-20 14:51:15 -0400

    completed

--- python.py
+++ python.py
@@ -1,22 +1,29 @@
-input = ['aaa', 'a', 'bb', 'ccc']
+input = ['aaa', 'aa', 'ccc', 'cc', 'b']
 
 def find(input):
-
-    result = list()
-    size = len(input)
     
-    # apprach: using selection sort
-    for i in range(size):
-        inx_of_min = i
-        temp = input[i]
-        for j in range(i+1, size):
-            if len(input[j]) < len(input[inx_of_min]):
-                inx_of_min  = j
-        input[i] = input[inx_of_min]
-        input[inx_of_min] = temp
-        result.append(input[i])
-    return result[0]
+    shortest = input[0]
+    
+    for string in input:
+        if len(string) < len(shortest):
+            shortest = string
+    
+    return shortest
 
-print(find(input))
+
+    # apprach: using selection sort; a bit long
+    # size = len(input)
+    # for i in range(size):
+    #     inx_of_min = i
+    #     temp = input[i]
+    #     for j in range(i+1, size):
+    #         if len(input[j]) < len(input[inx_of_min]):
+    #             inx_of_min  = j
+    #     input[i] = input[inx_of_min]
+    #     input[inx_of_min] = temp
+    #     result.append(input[i])
+    # return result[0]
+
+find(input)
 # print(input)
 # print(len('a'))
